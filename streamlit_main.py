937e2ef86620357acb738f51cf76ce07bde29a8b
Deekshith Vinayak <vinayakdeekshith@gmail.com>
2026-06-12 19:04:07 +0530

    Update streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -42,7 +42,7 @@ def set_bg(image_file):
 
     encoded = base64.b64encode(data).decode()
 
-    page_bg = f"""
+   page_bg = f"""
     <style>
 
     .stApp {{
@@ -55,6 +55,8 @@ def set_bg(image_file):
 
     </style>
     """
+
+    st.markdown(page_bg, unsafe_allow_html=True)
 
 
 # Your widgets below
